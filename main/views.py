5001a96ad3d788dfb5f9ddecc9e96a7fa646e209
nico0069xxx-sketch <nico0069xxx@gmail.com>
2026-01-20 15:59:16 +0100

    Update views.py

--- main/views.py
+++ main/views.py
@@ -974,7 +974,7 @@ def partner_landing(request, lang='ge'):
     return render(request, 'main/partner_landing.html', context)
 
 def professional_registration(request, lang='ge'):
-    success = True
+    success = False
     if request.method == 'POST':
         try:
             data = request.POST
